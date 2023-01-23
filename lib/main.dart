f9b3478dab4de4e0a5b3afefdfcbb432d54e5996
danaFlorescu <mariadana977@gmail.com>
2023-01-23 18:24:10 +0200

    Added new fonts and text

--- lib/main.dart
+++ lib/main.dart
@@ -24,7 +24,37 @@ class MyApp extends StatelessWidget {
               CircleAvatar(
                 radius: 100.0,
                 backgroundImage: AssetImage('images/profile_pic.jpg'),
-              )
+              ),
+              Center(
+                child: Text(
+                  "Dana M. Florescu",
+                  style: TextStyle(
+                    fontFamily: 'Pacifico',
+                    fontSize: 35.0,
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+              Center(
+                child: Text(
+                  "FLUTTER DEVELOPER",
+                  style: TextStyle(
+                    fontFamily: 'SourceCodePro',
+                    fontSize: 20.0,
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+              Center(
+                child: SizedBox(
+                  height: 20.0,
+                  width: 150.0,
+                  child: Divider(
+                    color: Colors.white,
+                  ),
+                ),
+              ),
             ],
           ),
         ),
