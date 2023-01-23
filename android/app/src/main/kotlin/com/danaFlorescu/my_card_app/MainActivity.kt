0722934dd12ad6dbbb007cae2035991741e9ef0a
danaFlorescu <mariadana977@gmail.com>
2023-01-23 17:34:24 +0200

    First commit - the basic structure

---
+++ android/app/src/main/kotlin/com/danaFlorescu/my_card_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.danaFlorescu.my_card_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Container(
+          decoration: BoxDecoration(
+              image: DecorationImage(
+                  image: AssetImage("images/background_img.jpg"),
+                  fit: BoxFit.cover)),
+        ),
+      ),
+    );
+  }
+}
